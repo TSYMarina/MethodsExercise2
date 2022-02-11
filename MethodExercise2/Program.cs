27c3ed72549495c1bbc39e0f6ce749278a3ab1f7
Marina Tsymlianskaia <marinatsy16@gmail.com>
2022-02-11 12:08:24 -0700

    * .gitignore: first commit
    * MethodExercise2.sln: first cmmit
    * Program.cs: first commit
    * MethodExercise2.csproj: first commit

---
+++ MethodExercise2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MethodExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Human! Looking for help with math? I can do that for you! Enter a whole numbers, please.");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("what is the second number?");
+            int num2 = int.Parse(Console.ReadLine());
+            int sum = Sum(num1, num2);
+            int multiplication = Multiplication(num1, num2);
+            Console.WriteLine($"Result of Sum: {sum}.");
+            Console.WriteLine($"Result of Multiplication: {multiplication}.");
+        }
+
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+    }
+}
+
+
+
