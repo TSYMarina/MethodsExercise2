e7a51768a2ad21b77c5f9a96c71b0baaaf4d47b3
Marina Tsymlianskaia <marinatsy16@gmail.com>
2022-02-11 14:00:23 -0700

    Added missing math functions

--- MethodExercise2/Program.cs
+++ MethodExercise2/Program.cs
@@ -12,9 +12,18 @@ namespace MethodExercise2
             int num2 = int.Parse(Console.ReadLine());
             int sum = Sum(num1, num2);
             int multiplication = Multiplication(num1, num2);
+            int division = Division(num1, num2);
+            int substraction = Substraction(num1, num2);
+            
             Console.WriteLine($"Result of Sum: {sum}.");
+            Console.WriteLine();
             Console.WriteLine($"Result of Multiplication: {multiplication}.");
-        }
+            Console.WriteLine();
+            Console.WriteLine($"Result of Division: {division}.");
+            Console.WriteLine();
+            Console.WriteLine($"Result of Substraction: {substraction}.");
+            Console.WriteLine();
+         }
 
         public static int Sum(int num1, int num2)
         {
@@ -23,6 +32,14 @@ namespace MethodExercise2
         public static int Multiplication(int num1, int num2)
         {
             return num1 * num2;
+        }
+        public static int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        public static int Substraction(int num1, int num2)
+        {
+            return num1 - num2;
         }
 
     }
